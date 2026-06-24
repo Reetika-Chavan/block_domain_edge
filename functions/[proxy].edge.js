51b6e29ace093fbd8a0b2a55b13f467e7bcde702
Reetika-Chavan <reetika.chavan@contentstack.com>
2026-06-24 15:18:08 +0530

    similar url

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,10 +1,22 @@
+const BLOCKED_PATHS = [
+  {
+    hostname: "blockdomainedge.devcontentstackapps.com",
+    pathname: "/contact",
+  },
+  {
+    hostname: "blockdomainedge.devcontentstackapps.com",
+    pathname: "/wp-content/uploads/2024/10/test-image-158x33.jpg",
+  },
+];
+
 export default function handler(request, _context) {
   const { hostname, pathname } = new URL(request.url);
 
-  if (
-    hostname === "blockdomainedge.devcontentstackapps.com" &&
-    pathname === "/contact"
-  ) {
+  const isBlocked = BLOCKED_PATHS.some(
+    (rule) => rule.hostname === hostname && rule.pathname === pathname
+  );
+
+  if (isBlocked) {
     return new Response("403 Forbidden - This page has been blocked.", {
       status: 403,
       headers: { "Content-Type": "text/plain" },
